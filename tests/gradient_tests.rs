9f6b369c29482f0d21cb081b0023ede5b1619eb2
Félix Fischer <felix91gr@gmail.com>
2018-06-24 22:32:31 -0400

    Started work on tests

---
+++ tests/gradient_tests.rs
@@ -0,0 +1,13 @@
+#[macro_use]
+extern crate quickcheck;
+
+// use gradient::create_gradient;
+
+extern crate perlin_noise;
+
+// quickcheck! {
+//   fn prop(xs: Vec<f32>) -> bool {
+
+//       create_gradient(xs) != xs
+//   }
+// }
